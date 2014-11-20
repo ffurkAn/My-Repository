b03fea6206619dda8012bdb66bd470337bf043e0
A. Furkan Tanriverdi <ffurkan.tanriverdi@gmail.com>
2014-11-20 14:08:07 +0200

    first commit

---
+++ com.unitbilisim.research.test/src/com/unitbilisim/research/test/MyClass.java
@@ -0,0 +1,16 @@
+package com.unitbilisim.research.test;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		
+		for(i = 0; i < 10; i++){
+			
+			System.out.println("i\n");
+		}
+	}
+
+}
