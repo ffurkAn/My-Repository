04c3c3a55107e8e3cc7f50d93da6451059c2c3c2
A. Furkan Tanriverdi <ffurkan.tanriverdi@gmail.com>
2014-11-29 15:06:33 +0200

    Person class added.

---
+++ com.unitbilisim.research.test/src/com/unitbilisim/research/test/Person.java
@@ -0,0 +1,29 @@
+package com.unitbilisim.research.test;
+
+public class Person {
+
+	protected String name;
+	protected String surname;
+	protected int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
